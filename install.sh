29b3fb80bd86e670fef5adbff65d2e4c6e2ad693
salencebg <gosevskibg@gmail.com>
2013-12-17 19:24:58 +0100

    Fixed install

--- install.sh
+++ install.sh
@@ -3,11 +3,18 @@
 home_dir=/Users/aleksandargosevski
 dotfiles_dir=$home_dir/dotfiles
 
-#loop through files and link them
+# loop through old files and delete them
 for file in vimrc zshrc aliases osx vim
 do
-    echo "Removing $file."
+     echo "Removing old $file."
     rm -rf $dotfiles_dir/$file $home_dir/.$file
+done
+
+echo "Finished deleting."
+
+# loop through files and link them
+for file in vimrc zshrc aliases osx vim
+do
     echo "Linking $file."
     ln -s $dotfiles_dir/$file $home_dir/.$file
 done
