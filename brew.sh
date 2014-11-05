e7ea8768bbec0f68fe36a2b544f6373600e4c39c
Aleksandar Gosevski <gosevski@vast.com>
2014-11-05 15:13:29 +0100

    Add brew install

---
+++ brew.sh
@@ -0,0 +1,6 @@
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew doctor
+brew update
+brew install ack wget caskroom/cask/brew-cask git node mysql php56 php56-mcrypt composer
+brew install vim --with-lua
+brew cask install appcleaner adium caffeine google-chrome iterm2 slate sparrow vlc alfred cloud hyperswitch sketch sourcetree xtrafinder tunnelblick
