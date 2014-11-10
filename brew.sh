bdb174f4c288066fb5f1760bb03abe0127397cbc
Aleksandar Gosevski <gosevski@vast.com>
2014-11-10 22:41:06 +0100

    Add custom tap for popcorn

--- brew.sh
+++ brew.sh
@@ -1,6 +1,7 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew doctor
 brew update
+brew tap casidiablo/homebrew-custom
 brew install ack wget ctags caskroom/cask/brew-cask git node mysql php56 php56-mcrypt composer
 brew install vim --with-lua
 brew cask install appcleaner adium caffeine google-chrome iterm2 slate sparrow vlc alfred cloud hyperswitch sketch sourcetree xtrafinder tunnelblick
