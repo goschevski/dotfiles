e96ade99623a4abd4158429833e03cc6e9ce3cf8
Aleksandar Gosevski <gosevski@vast.com>
2014-11-11 23:09:21 +0100

    Add skype to brew cask

--- brew.sh
+++ brew.sh
@@ -4,4 +4,4 @@ brew update
 brew tap casidiablo/homebrew-custom
 brew install ack wget ctags caskroom/cask/brew-cask git node mysql php56 php56-mcrypt composer
 brew install vim --with-lua
-brew cask install appcleaner adium caffeine google-chrome iterm2 slate sparrow vlc alfred cloud hyperswitch sketch sourcetree xtrafinder tunnelblick
+brew cask install appcleaner adium skype caffeine google-chrome iterm2 slate sparrow vlc alfred cloud hyperswitch sketch sourcetree xtrafinder tunnelblick
