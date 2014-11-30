e98519d897f765b4cdb663afda995d73d2090f62
Aleksandar Gosevski <gosevskibg@gmail.com>
2014-11-30 02:09:28 +0100

    Add quick look plugins to cask task

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -7,6 +7,6 @@ brew install ack wget ctags node caskroom/cask/brew-cask
 echo "\033[0;34mBrew install vim...\033[0m"
 brew install vim --with-lua
 echo "\033[0;34mBrew cask install apps...\033[0m"
-brew cask install adium appcleaner cloud hyperswitch sketch slate sparrow twitterrific xtrafinder alfred caffeine google-chrome iterm2 skype sourcetree tunnelblick vlc
+brew cask install adium appcleaner cloud hyperswitch sketch slate sparrow twitterrific xtrafinder alfred caffeine google-chrome iterm2 skype sourcetree tunnelblick vlc qlcolorcode qlstephen qlmarkdown quicklook-json
 echo "\033[0;34mNPM install global modules...\033[0m"
 sudo npm i -g gulp grunt-cli jscs jshint nd n json david bower express-generator
