d1ccdfb8fd50828862ac440080addff527f665cd
Aleksandar Gosevski <gosevski@vast.com>
2017-01-08 16:56:58 +0100

    Minor installation changes

--- install.sh
+++ install.sh
@@ -34,7 +34,6 @@ brew install node
 brew install imgur-screenshot
 brew install w3m
 brew install tmux
-brew install python3
 brew install diff-so-fancy
 brew install httpie
 brew install switchaudio-osx
@@ -44,7 +43,6 @@ brew install youtube-dl
 brew install ical-buddy
 brew install caskroom/cask/brew-cask
 brew install vim --with-lua
-sudo mv /usr/bin/vim /usr/bin/vim73
 brew linkapps
 
 echoGreen "5. Installing apps using brew cask..."
@@ -70,23 +68,24 @@ echoGreen "6. Installing node global modules..."
 npm i -g gulp
 npm i -g eslint
 npm i -g n
-npm i -g bower
 npm i -g how2
 npm i -g nodemon
+npm i -g yarn
 
-echoGreen "7. Setup homefiles..."
+echoGreen "7. Install and setup oh-my-zsh..."
+git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+mkdir ~/.oh-my-zsh/custom/themes/
+ln -s ~/dotfiles/goschevski.zsh-theme ~/.oh-my-zsh/custom/themes/goschevski.zsh-theme
+
+echoGreen "8. Setup homefiles..."
 for file in $(ls ~/dotfiles/homefiles/)
 do
     rm -rf ~/.$file
     ln -s ~/dotfiles/homefiles/$file ~/.$file
 done
 
-echoGreen "8. Setup vim"
+echoGreen "9. Setup vim"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qall
 
-echoGreen "9. Install and setup oh-my-zsh..."
-git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-mkdir ~/.oh-my-zsh/custom/themes/
-ln -s ~/dotfiles/goschevski.zsh-theme ~/.oh-my-zsh/custom/themes/goschevski.zsh-theme
