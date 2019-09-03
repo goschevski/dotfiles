fbd4c557d5ceaee6b3db6315a507ace2bd4641d7
Aleksandar Gosevski <gosevskibg@gmail.com>
2019-09-03 14:59:43 -0700

    Install script cleanup

--- install.sh
+++ install.sh
@@ -16,7 +16,6 @@ sh ~/dotfiles/bin/osx.sh 2>&1 > /dev/null
 colorEcho "Installing home brew..."
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap homebrew/core
-brew tap homebrew/versions
 brew tap caskroom/fonts 
 brew tap jesseduffield/lazygit
 
@@ -45,7 +44,7 @@ brew install coreutils
 brew install htop
 brew install noti
 brew install pidof
-brew install gnu-sed --default-names
+brew install gnu-sed
 brew install wget
 brew install node
 brew install imgur-screenshot
@@ -55,62 +54,37 @@ brew install diff-so-fancy
 brew install python3
 brew install httpie
 brew install siege
-brew install pandoc
-brew install asciinema
-brew install cmus
-brew install mpd
-brew install ncmpcpp
-brew install switchaudio-osx
 brew install reattach-to-user-namespace
 brew install youtube-dl
 brew install ical-buddy
-brew install dasht
-brew install macvim --with-lua
-brew install vim --with-lua
-brew linkapps --local macvim
-pip install mdv
+brew install vim
+brew install jq
 
 colorEcho "Installing apps using brew cask..."
 brew cask install alfred
-brew cask install appcleaner
 brew cask install firefox-nightly
 brew cask install google-chrome
-brew cask install google-drive
-brew cask install iterm2-nightly
+brew cask install google-backup-and-sync
+brew cask install iterm2
 brew cask install mattr-slate
 brew cask install qlcolorcode
 brew cask install qlmarkdown
 brew cask install qlstephen
 brew cask install quicklook-json
-brew cask install skype
 brew cask install transmission
 brew cask install tunnelblick
-brew cask install virtualbox
 brew cask install font-iosevka
 brew cask install font-fira-code
 brew cask install font-victor-mono
 
 colorEcho "Installing node global modules..."
-npm i -g gulp
-npm i -g npmf
 npm i -g eslint
 npm i -g n
-npm i -g how2
-npm i -g vtop
 npm i -g nodemon
-npm i -g yarn
-
-colorEcho "Install dasht docets..."
-dasht-docsets-install BackboneJS
-dasht-docsets-install JavaScript
-dasht-docsets-install Lo-Dash
-dasht-docsets-install NodeJS
-dasht-docsets-install Vim
 
 colorEcho "Install and setup oh-my-zsh..."
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-mkdir ~/.oh-my-zsh/custom/themes/
 ln -s ~/dotfiles/templates/goschevski.zsh-theme ~/.oh-my-zsh/custom/themes/goschevski.zsh-theme
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
@@ -126,19 +100,27 @@ colorEcho "Setup vim"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qall
 
-colorEcho "Setup cmus"
-pip3 install mutagen pyobjc
-git clone https://github.com/azadkuh/cmus-osx.git ~/.cmus-osx
-~/.cmus-osx/setup.py install
 
-colorEcho "Setup mpd and ncmpcpp"
-mkdir -p ~/.mpd
-mkdir -p ~/.ncmpcpp
-ln -s ~/dotfiles/homefiles/ncmpcpp ~/.ncmpcpp/bindings
-ln -s ~/dotfiles/homefiles/mpd.conf ~/.mpd/mpd.conf
-touch ~/.mpd/mpd.db ~/.mpd/mpd.log ~/.mpd/mpdstate ~/.mpd/mpd.log
+colorEcho "Install ansi"
+curl -OL git.io/ansi
+chmod 755 ansi
+sudo mv ansi /usr/local/bin/
+
+# Italic in iterm
+colorEcho "Setup italic"
+tic -x ~/dotfiles/templates/xterm-256color-italic.terminfo
+tic -x ~/dotfiles/templates/tmux-256color.terminfo
 
 colorEcho "Setup hosts files"
 sudo su -
 echo "0 15 * * * /Users/gosevski/dotfiles/bin/generateHosts" > /tmp/mycron
 crontab /tmp/mycron
+
+# Tmux window on C-1, C-2, etc.
+# iTerm config -> Profiles -> Keys
+# ^1 -> Send Escape Sequence [27;5;49~
+# ^2 -> Send Escape Sequence [27;5;50~
+
+# Tmux cmd+K
+# iTerm config + Profiles -> Keys
+# ⌘ k -> Send Hex Codes 0x1d
