7b43587adced91dfcbab69d4f53ea81daebdca9a
Aleksandar Gosevski <gosevskibg@gmail.com>
2022-11-13 22:12:48 +0100

    highlight text on yank

--- nvim/lua/goschevski/core/options.lua
+++ nvim/lua/goschevski/core/options.lua
@@ -28,3 +28,14 @@ opt.signcolumn = "yes"
 opt.splitright = true
 -- split horizontal window to the bottom
 opt.splitbelow = true 
+
+-- Highlight on yank
+local ag = vim.api.nvim_create_augroup
+local au = vim.api.nvim_create_autocmd
+au('TextYankPost', {
+  group = ag('yank_highlight', {}),
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank { higroup='IncSearch', timeout=300 }
+  end,
+})
