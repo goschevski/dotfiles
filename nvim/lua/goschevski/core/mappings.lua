d3a0a71d63c73d7b1ee5b8a693336a6daaa40d71
Aleksandar Goševski <gosevskibg@gmail.com>
2022-12-14 17:19:12 +0100

    Add mappings labels

--- nvim/lua/goschevski/core/mappings.lua
+++ nvim/lua/goschevski/core/mappings.lua
@@ -3,25 +3,35 @@ vim.g.maplocalleader=" "
 
 local keymap = vim.keymap
 
+-- reload vim config
 keymap.set('n', '<leader><leader>r', ':lua ReloadConfig()<CR>')
+-- paste from 0 register
 keymap.set('x', '<Leader>v', '"0p')
+-- chords
 keymap.set('n', '<Leader><Leader>c', 'magg:vsplit<CR>:set noscrollbind<CR><C-w>l<C-f>:set scrollbind<CR><C-w>h:set scrollbind<CR>`a')
+-- close all splits except from the focused one
 keymap.set('n', '=', ':only<CR>')
 
+-- format json
 keymap.set('n', '<Leader><Leader>j', ':%!python3 -m json.tool<CR>')
 keymap.set('v', '<Leader><Leader>j', ':!python3 -m json.tool<CR>')
 
-keymap.set('n', 'Q', '<NOP>')
+-- execute q macro
+keymap.set('n', 'Q', '@q')
 
+-- copy file path
 keymap.set('n', '<Leader>y', ':let @* = expand("%")<CR>')
+-- paste instead of line
 keymap.set('n', '<Leader>p', '^v$p')
 
+-- go to start or end of the line
 keymap.set('n', 'Y', 'y$')
 keymap.set('n', 'L', '$')
 keymap.set('v', 'L', '$h')
 keymap.set('n', 'H', '^')
 keymap.set('v', 'H', '^')
 
+-- quick list and location list
 keymap.set('n', '<Leader>co', ':copen<CR>')
 keymap.set('n', '<Leader>cc', ':cclose<CR>')
 keymap.set('n', '<Leader>cn', ':cn<CR>')
@@ -31,60 +41,79 @@ keymap.set('n', '<Leader>lc', ':lclose<CR>')
 keymap.set('n', '<Leader>ln', ':ln<CR>')
 keymap.set('n', '<Leader>lp', ':lp<CR>')
 
+-- search selection
 keymap.set('v', '//', 'y/<C-R>"<CR>N')
 
+-- move selection
 keymap.set('v', 'J', ':m \'>+1<CR>gv=gv')
 keymap.set('v', 'K', ':m \'<-2<CR>gv=gv')
 
+-- search things
 keymap.set('n', '\\', '*N')
 keymap.set('n', '\'', '*Ncgn')
 keymap.set('n', 'n', 'nzz')
 keymap.set('n', 'N', 'Nzz')
 
+-- command mode shortcut
 keymap.set('n', ';', ':')
 keymap.set('v', ';', ':')
 
+-- switch buffers
 keymap.set('n', '<Leader><Tab>', '<C-^>')
 
+-- delete all buffers
 keymap.set('n', '<Leader>Q', ':bufdo bdelete<CR>')
+-- delete current buffer
 keymap.set('n', '<Leader>x', ':bn<CR>:bd#<CR>')
 
+-- true line up/down
 keymap.set('n', 'j', 'gj')
 keymap.set('n', 'k', 'gk')
 
+-- delete and yank file
 keymap.set('n', '<Leader>d', 'ggdG')
 keymap.set('n', '<Leader><Leader>y', 'mpggVGy`p')
 
-
+-- add line up/down
 keymap.set('n', '<Leader>j', 'o<Esc>k')
 keymap.set('n', '<Leader>k', 'O<Esc>j')
 
+-- add marks when moving
 keymap.set('n', 'gg', 'mpgg')
 keymap.set('n', 'G', 'mpG')
 keymap.set('n', '/', 'mp/')
 
+-- fast escape from insert mode
 keymap.set('i', 'jj', '<Esc>')
 keymap.set('i', 'jk', '<Esc>')
 
+-- search and replace shortcut
 keymap.set('n', '<Leader>sr', ':%s///<left><left><C-f>i')
 
+-- fast save
 keymap.set('n', '<Leader>w', ':w<CR>')
+-- fast quit
 keymap.set('n', '<Leader>q', ':q<CR>')
 
+-- resize splits
 keymap.set('n', '<Leader>,', '<C-w>10<')
 keymap.set('n', '<Leader>.', '<C-w>10>')
 
+-- delete highlight
 keymap.set('n', '<BS>', ':noh<CR>')
 
+-- indentation
 keymap.set('x', '<s-tab>', '<gv')
 keymap.set('x', '<tab>', '>gv')
 keymap.set('n', '<tab>', '>>')
 keymap.set('n', '<s-tab>', '<<')
 
+-- fast switch between vue component files
 keymap.set('n', '<Leader>1', ':e %<.vue<CR>')
 keymap.set('n', '<Leader>2', ':e %<.js<CR>')
 keymap.set('n', '<Leader>3', ':e %<.css<CR>')
 
+-- line object
 keymap.set('x', 'il', 'g_o^')
 keymap.set('o', 'il', ':normal vil<CR>')
 keymap.set('x', 'al', '$o^')
