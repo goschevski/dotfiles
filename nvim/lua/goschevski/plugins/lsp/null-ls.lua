5ec4e3f2e062e370231410d9c7f35672f2abe6ea
Aleksandar Goševski <gosevskibg@gmail.com>
2023-02-09 13:40:46 +0100

    Updates

--- nvim/lua/goschevski/plugins/lsp/null-ls.lua
+++ nvim/lua/goschevski/plugins/lsp/null-ls.lua
@@ -6,6 +6,10 @@ end
 local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
 
 null_ls.setup({
+  sources = {
+    null_ls.builtins.formatting.eslint,
+    null_ls.builtins.diagnostics.eslint,
+  },
   on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
       vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
