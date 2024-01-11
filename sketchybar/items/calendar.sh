d33a34a37e078917200c1e1391ba119762e26d7c
Aleksandar Goševski <gosevskibg@gmail.com>
2024-01-11 17:50:11 +0100

    add spacing for calendar item in sketchybar

--- sketchybar/items/calendar.sh
+++ sketchybar/items/calendar.sh
@@ -3,5 +3,6 @@
 sketchybar --add item calendar right \
            --set calendar icon.color=$MAGENTA \
                           icon.font="$FONT:ExtraBold:13.0" \
+                          background.padding_left=20 \
                           update_freq=30 \
                           script="$PLUGIN_DIR/calendar.sh"
