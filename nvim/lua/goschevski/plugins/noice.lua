7d616c0b0d8243a2cdbfc945ada77b1893f2a046
Aleksandar Goševski <gosevskibg@gmail.com>
2024-01-30 20:56:53 +0100

    add noice

---
+++ nvim/lua/goschevski/plugins/noice.lua
@@ -0,0 +1,18 @@
+return {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	opts = {
+		lsp = {
+			-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+				["vim.lsp.util.stylize_markdown"] = true,
+				["cmp.entry.get_documentation"] = true,
+			},
+		},
+	},
+	dependencies = {
+		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+		"MunifTanjim/nui.nvim",
+	},
+}
