ff2b57aa0208ed8ebeeffefd8c200b1734962ee8
Aleksandar Goševski <gosevskibg@gmail.com>
2024-04-17 00:09:17 +0200

    feat(neovim): update lazy setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	vim.fn.system({
 		"git",
 		"clone",
@@ -14,6 +14,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("goschevski.plugins", { change_detection = { enabled = false } })
+require("lazy").setup("goschevski.plugins", { change_detection = { notify = false } })
 require("goschevski.core.options")
 require("goschevski.core.mappings")
